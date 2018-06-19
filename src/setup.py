ca8e422e100c18491e415e890c3cbbaae507faea
Xueyou Luo <xueyou.luo@aidigger.com>
2018-06-19 17:07:05 +0800

    Update setup.py

--- src/setup.py
+++ src/setup.py
@@ -1,8 +1,21 @@
 from distutils.core import setup
 from Cython.Build import cythonize
 from distutils.extension import Extension
-import numpy
+from Cython.Distutils import build_ext
 
+class CustomBuildExtCommand(build_ext):
+    """build_ext command for use when numpy headers are needed."""
+    def run(self):
+
+        # Import numpy here, only when headers are needed
+        import numpy
+
+        # Add numpy headers to include_dirs
+        self.include_dirs.append(numpy.get_include())
+
+        # Call original build_ext command
+        build_ext.run(self)
+        
 sourcefiles  = ['sent2vec.pyx', 
                 'fasttext.cc', 
                 "args.cc", 
@@ -20,10 +33,12 @@ ext=[Extension('*',
             sourcefiles,
             extra_compile_args=compile_opts,
             language='c++',
-            include_dirs=[numpy.get_include()])]
+            include_dirs=[])]
 
 setup(
   name='sent2vec',
+  cmdclass = {'build_ext': CustomBuildExtCommand},
+  install_requires=['numpy'],
   ext_modules=cythonize(ext)
 )
 
